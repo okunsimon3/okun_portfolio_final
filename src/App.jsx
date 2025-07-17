0619b1d78a94a0f16a9f0362101e2a3ea727721b
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2025-07-16 19:08:28 -0500

    paragraph change

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello my name is Simon
         </p>
       </div>
       <p className="read-the-docs">
