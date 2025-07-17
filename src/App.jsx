b2e9c4a7a77220d6eac29e2ce9ae46179b7dfe68
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2025-07-16 19:10:44 -0500

    try again

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Hello my name is Simon
+          Hello my name is Simon hello
         </p>
       </div>
       <p className="read-the-docs">
