38483ee9e6970325a97213652ca9907cd28a7ca7
okunsimon3 <okunsimon3@gmail.com>
2025-07-21 19:55:19 -0500

    Home finalizations

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -7,12 +7,17 @@ export default function HomeContent() {
         <div className="home-content">
             <div className="home-grid-left">
                 <h1>Simon Okun</h1>
-                <h3>Digital Marketing Specialist</h3>
+                <h2>Digital Marketing Specialist</h2>
             </div>
             <div className="home-grid-right">
                 <img src={Headshot} id='home-headshot'/>
             </div>
         </div>
+        <div className='circle-1'></div>
+        <div className='circle-2'></div>
+        <div className='circle-3'></div>
+        <div className='circle-4'></div>
+        <div className='circle-5'></div>
         </>
     )
 }
