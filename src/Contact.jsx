3e70b2104c6a55c772e7f41d2429b9699f40a36b
okunsimon3 <okunsimon3@gmail.com>
2025-08-08 17:42:23 -0500

    added redirects

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -21,7 +21,7 @@ export default function Contact() {
                 <form className='contact-form' 
                     name='contact'
                     method='POST'
-                    netlify
+                    data-netlify="true"
                 >
                     <input type="hidden" name="form-name" value="contact" />
                     <div>
