f6cd27d188ca984a4fa1abf8a609e21b6be6acff
okunsimon3 <okunsimon3@gmail.com>
2025-08-08 17:53:01 -0500

    contact updates

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,50 +3,39 @@ import { NavIcon } from './components/NavIcon'
 import './Contact.css'
 import './Mobile.css'
 
-export default function Contact() {
-    return (
-        <>
+const Contact = () => (
+    <div className="contact-page">
         <NavIcon />
-        <h1>Contact</h1>
-        <div className="contact-grid">
-            <div className="contact-grid-left">
-                <h2>Get In Touch</h2>
-                <ul>
-                    <li><a href='mailto:okunsimon3@gmail.com' target='blank'>okunsimon3@gmail.com</a></li>
-                    <li><a href='tel:18473700720'>(847) 370 - 0720</a></li>
-                    <li><a href='https://www.linkedin.com/in/simonokun/' target='blank'>LinkedIn</a></li>
-                </ul>
+        <div className='contact-container'>
+            <div className='contact-left'>
+                <h1>Get In Touch</h1>
             </div>
-            <div className="contact-grid-right">
-                <form className='contact-form' 
-                    name='contact'
-                    method='POST'
-                    data-netlify="true"
-                >
-                    <input type="hidden" name="form-name" value="contact" />
-                    <div>
-                        <h3 className='contact-first-text'>First Name</h3>
-                        <label>
-                            <input type="text" name="first-name" required/>
-                        </label>
-                    </div>
-                    <div>
-                        <h3>Email</h3>
-                        <label>
-                            <input type="email" name="email" required/>
-                        </label>
-                    </div>
-                    <div>
-                        <h3>More Info</h3>
-                        <label>
-                            <textarea name="message"></textarea>
-                        </label>
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+            <form className='contact-form' 
+            name='contact'
+            method='POST'
+            data-netlify='True'
+            action="/success"
+            >
+                <input type="hidden" name="form-name" value="contact" />
+                <div>
+                    <label>First Name
+                        <input type="text" name="first-name" />
+                    </label>
+                </div>
+                <div>
+                    <label>Email
+                        <input type="email" name="email" />
+                    </label>
+                </div>
+                <div>
+                    <label>More Info
+                        <textarea name="message"></textarea>
+                    </label>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </div>
-        <Footer />
-        </>
-    )
-}
+    </div>
+);
+
+export default Contact;
